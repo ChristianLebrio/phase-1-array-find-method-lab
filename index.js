e463106fa01e2cccd41003e3e8e4a7fd22ea8404
Christian Lebrio <ChristianLebrio@gmail.com>
2023-01-20 22:35:32 -0500

    solved tests

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // code your solution here
+
+  function superbowlWin(recordsArray) {
+    const result = recordsArray.find(record => record.result === "W");
+    if(result !== undefined){
+        return result.year
+    }
+    else{
+        return result
+    }
+  }
